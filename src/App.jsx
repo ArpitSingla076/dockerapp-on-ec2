9265c16bdec1b9ddbd8509ada80f6c6df3cde228
Arpit Singla <arpitsingla076@gmail.com>
2026-03-18 18:21:19 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome to my React App</h1>
+      <form className='form' centered="true">
+        <label htmlFor='name'>Name:</label>
+        <input type='text' id='name' name='name' />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
